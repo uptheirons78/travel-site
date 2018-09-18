75460cb72b7121a506faae3da482e101bde7710d
Mauro Bono <bono.mauro@gmail.com>
2018-09-18 10:06:06 +0200

    organize gulp, add browser-sync and error handling

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,24 @@
+const gulp = require('gulp');
+const watch = require('gulp-watch');
+const browserSync = require('browser-sync').create();
+
+gulp.task('watch', () => {
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir: 'app'
+    }
+  });
+
+  watch('./app/index.html', () => {
+    browserSync.reload();
+  });
+
+  watch('./app/assets/styles/**/*.css', () => {
+    gulp.start('cssInject');
+  });
+});
+
+gulp.task('cssInject', ['styles'], () => {
+  return gulp.src('./app/temp/styles/style.css').pipe(browserSync.stream());
+});
