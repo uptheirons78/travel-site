db46c4681976e630fc91254e3aaefe8077651828
Mauro Bono <bono.mauro@gmail.com>
2018-09-18 11:37:44 +0200

    add postcss mixin and make hero image responsive using picture tag and srcset attributes

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -4,11 +4,12 @@ const autoprefixer = require('autoprefixer');
 const cssvars = require('postcss-simple-vars');
 const nested = require('postcss-nested');
 const cssImport = require('postcss-import');
+const mixins = require('postcss-mixins');
 
 gulp.task('styles', () => {
   return gulp
     .src('./app/assets/styles/style.css')
-    .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
+    .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
     .on('error', function(errorInfo) {
       console.log(errorInfo.toString());
       this.emit('end');
