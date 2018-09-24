c5c4ae98dab7be498ac8823244aa67ce2151fb3c
Mauro Bono <bono.mauro@gmail.com>
2018-09-24 11:08:58 +0200

    finished sticky header on medium and large screen with smooth scroll and waypoints

---
+++ app/assets/scripts/modules/StickyHeader.js
@@ -0,0 +1,65 @@
+import $ from 'jquery';
+import 'waypoints/lib/noframework.waypoints';
+import smoothScroll from 'jquery-smooth-scroll';
+
+class StickyHeader {
+  constructor() {
+    this.siteHeader = $('.site-header');
+    this.headerTriggerElement = $('.large-hero__title');
+    this.createHeaderWaypoints();
+    this.pageSections = $('.page-section');
+    this.headerLinks = $('.primary-nav a');
+    this.createPageSectionWaypoints();
+    this.addSmoothScrolling();
+  }
+
+  addSmoothScrolling() {
+    this.headerLinks.smoothScroll();
+  }
+
+  createHeaderWaypoints() {
+    let k = this;
+    new Waypoint({
+      element: this.headerTriggerElement[0],
+      handler: direction => {
+        if (direction == 'down') {
+          k.siteHeader.addClass('site-header--dark');
+        } else {
+          k.siteHeader.removeClass('site-header--dark');
+        }
+      }
+    });
+  }
+
+  createPageSectionWaypoints() {
+    let that = this;
+    this.pageSections.each(function() {
+      let currentPageSection = this;
+
+      new Waypoint({
+        element: currentPageSection,
+        handler: direction => {
+          if (direction == 'down') {
+            let matchingHeaderLink = currentPageSection.getAttribute('data-matching-link');
+            that.headerLinks.removeClass('is-current-link');
+            $(matchingHeaderLink).addClass('is-current-link');
+          }
+        },
+        offset: '18%'
+      });
+      new Waypoint({
+        element: currentPageSection,
+        handler: direction => {
+          if (direction == 'up') {
+            let matchingHeaderLink = currentPageSection.getAttribute('data-matching-link');
+            that.headerLinks.removeClass('is-current-link');
+            $(matchingHeaderLink).addClass('is-current-link');
+          }
+        },
+        offset: '-40%'
+      });
+    });
+  }
+}
+
+export default StickyHeader;
